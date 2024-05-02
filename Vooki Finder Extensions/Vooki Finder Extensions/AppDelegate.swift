9cae23e1d873a93dbf808d85708f4dcb5c7544d6
Michal Duda <github@vookimedlo.cz>
2024-05-02 22:53:24 +0200

    🔩Code: Initial app

---
+++ Vooki Finder Extensions/Vooki Finder Extensions/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Vooki Finder Extensions
+//
+//  Created by Michal Duda on 02.05.2024.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
